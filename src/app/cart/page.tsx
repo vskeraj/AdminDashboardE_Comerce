a8015edc8e460cd05c179df3bd52c97abe2915f1
Rinapaa <rp69888@ubt-uni.net>
2026-01-07 12:45:35 +0100

    fix

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@
 
 import PaymentForm from "@/components/PaymentForm";
 import ShippingForm from "@/components/ShippingForm";
-import { CartItemsType } from "@/types"
+import { CartItemsType, ShippingFormInputs } from "@/types"
 import { ArrowRight, Trash2 } from "lucide-react";
 import { useSearchParams } from "next/navigation"
 import { useRouter } from "next/navigation";
@@ -88,7 +88,7 @@ const cartItems:CartItemsType = [
 const CartPage = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
-    const [shippingForm,setShippingForm] = useState(null);
+    const [shippingForm,setShippingForm] = useState<ShippingFormInputs | null>(null); 
     const activeStep = parseInt(searchParams.get("step") || "1");
     return (
          <div className="flex flex-col gap-8 items-center justify-center mt-12">
